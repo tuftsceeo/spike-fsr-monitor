1ce3ca3f79db50a04d1576ec4e959e9db01bee51
anditaco <trioquark3@gmail.com>
2019-07-18 10:08:24 -0400

    made highest value persist for 3 seconds before clearing

--- spike/script.py
+++ spike/script.py
@@ -1,15 +1,24 @@
-import hub,utime
+import hub,utime, time
 
 hub.port.B.info()
 # if info comes back with None - then you have to restart the microprocessor
 
-
+loopTime = 3
+loopPosition = 0.0
+max = 0
+startTime = time.time()
 while True:  
      try:
+          loopPosition = loopPosition + time.time() - startTime
+          startTime = time.time()
           value = hub.port.B.device.get()[0]
-          hub.display.show(str(value))
-
-          utime.sleep(0.1)
+          print(value, float(loopPosition))
+          if value >= max or loopPosition >= loopTime:
+               max = value
+               loopPosition = 0
+          
+          hub.display.show(str(max))
+          utime.sleep(0.05)
 
      except:
-          utime.sleep(1)
+          utime.sleep(0.5)
